0a7365587b818e744bbbd2454cec1897488b27b7
erin.major <erinmajor254@gmail.com>
2026-01-27 19:42:24 -0700

    complete registration view

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -6,5 +6,6 @@ const account = require("../controllers/accountController")
 
 // Route to get login view
 router.get("/login", utilities.handleErrors(account.buildLogin))
+router.get("/register", utilities.handleErrors(account.buildRegister))
 
 module.exports = router;
